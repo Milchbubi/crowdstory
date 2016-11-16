1faab6ce7a7238a7d74a6640fb450f8f29568aa4
Michael <michael.speitelsbach@gmail.com>
2016-11-16 18:58:39 +0100

    added sourcecode of remaining classes to version control

---
+++ src/main/java/eWorld/Main.java
@@ -0,0 +1,38 @@
+package eWorld;
+
+/**
+ * 
+ * @author michael
+ * TODO? outsource this class in another project?
+ */
+public class Main {
+
+	// finals
+	
+//	private static final String DATABASE_NODE = "127.0.0.1";
+	
+	
+	// attributes
+	
+//	private static final Gate DB_GATE = new ApacheCassandraGate(DATABASE_NODE);
+	
+	
+	// methods
+	
+	public static void main(String[] args) {
+		
+		// open shell
+//		Shell shell = new Shell(DB_GATE);
+//		shell.doCommands();
+		
+		// fill database
+//		try {
+//			fillDB();
+//		} catch (EvRequestException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		DB_GATE.closeDatabase();
+	}
+	
+}
